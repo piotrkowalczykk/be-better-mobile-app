35794c31c955ba171a4f46cb3ae7d754d233abfd
piotrkowalczykk <kowalczyk.piotrt@gmail.com>
2024-07-05 13:38:37 +0200

    Added task display layout and delete task option

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,21 @@
-import { Text, SafeAreaView, StyleSheet, Image, View} from 'react-native';
+
+import { Text, SafeAreaView, StyleSheet, Image, View, FlatList, ActivityIndicator, TouchableOpacity} from 'react-native';
 import { getData } from '../components/StorageHelper';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DefaultImage from '../assets/user.png'
 import DateNavigator from '../components/DateNavigator';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { createTable, getAllRows, insertData, openDatabase } from '../components/Database';
+import { clearTable, createTable, getAllRows, openDatabase, deleteData } from '../components/Database';
+import { useFocusEffect } from "@react-navigation/native";
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function HomeScreen(){
 
     const DEFAULT_IMAGE = Image.resolveAssetSource(DefaultImage).uri;
     const [username, setUsername] = useState('');
-    const [userAvatar, setUserAvatar] = useState(DEFAULT_IMAGE)
+    const [userAvatar, setUserAvatar] = useState(DEFAULT_IMAGE);
+    const [tasksList, setTasksList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const getUserData = async () => {
         try {
@@ -27,13 +32,27 @@ export default function HomeScreen(){
         const db = await openDatabase();
         await createTable(db);
         const rows = await getAllRows(db);
+        setTasksList(rows);
+        setIsLoading(false);
     }
 
+    const deleteTask = async (id) => {
+        const db = await openDatabase();
+        await deleteData(db, id)
+        const updatedTasks = tasksList.filter(task => task.id !== id);
+        setTasksList(updatedTasks);
+    }
 
     useEffect(() => {
         getUserData();
         startDb();
     }, [])
+
+    useFocusEffect(
+        React.useCallback(() => {
+            startDb();
+        }, [])
+    );
 
     return (
         <SafeAreaView style={styles.container}>
@@ -44,7 +63,22 @@ export default function HomeScreen(){
             {/* <Image source={{uri: userAvatar}} style={styles.avatar} />
             <Text>{username}</Text> */}
             <View style={styles.tasksContainer}>
-                <Text>,,,</Text>
+                    {isLoading ? 
+                        ( <ActivityIndicator size="large" color="#fff"/> ) :
+                        ( 
+                            <FlatList style={styles.tasksList}
+                            data={tasksList}
+                            renderItem={({ item }) => {
+                            return (
+                            <View style={styles.card} key={item.id}>
+                                <Text style={styles.cardText}>{item.name}</Text>
+                                <TouchableOpacity onPress={() => deleteTask(item.id)}>
+                                    <MaterialIcons name='check' color='#d2d9d8' size={wp('6%')}/>
+                                </TouchableOpacity>
+                            </View> )}}
+                            />
+                        )
+                    }  
             </View>
         </SafeAreaView>
     );
@@ -77,5 +111,22 @@ const styles = StyleSheet.create({
         width: wp('100%'),
         backgroundColor: '#2c2626',
         alignItems: 'center'
+    },
+    tasksList: {
+        width: '80%'
+    },
+    card: {
+        width: '100%',
+        borderRadius: 15,
+        padding: 20,
+        marginBottom: 10,
+        backgroundColor: '#211e1e',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+        
+    },
+    cardText: {
+        color: 'white'
     }
 })
